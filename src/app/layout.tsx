9ec4b03400576f7c942ebae929f968cf18fd763b
Nopnapat Norasri <nopnapatn@gmail.com>
2024-11-27 20:07:34 +0700

    fix(*): run lint fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
-import { ThemeProvider } from "@/conponents/theme-provider"
 import type { Metadata } from "next"
 import localFont from "next/font/local"
+
+import { ThemeProvider } from "@/conponents/theme-provider"
 import "./globals.css"
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900"
 })
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900"
 })
@@ -25,10 +26,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-    >
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
